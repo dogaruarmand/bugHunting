0c01fe113e2b87d953fd6c42092f53190c9fa2c2
Armand Dogaru <dogaruionelarmand@gmail.com>
2023-07-22 13:33:54 +0300

    first commit

---
+++ src/main/java/com/endava/bugHunting/bug_hunting/BugHuntingApplication.java
@@ -0,0 +1,13 @@
+package com.endava.bugHunting.bug_hunting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugHuntingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugHuntingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endava/bugHunting/bug_hunting/BugHuntingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.bugHunting.bug_hunting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugHuntingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
