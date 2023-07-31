6188c49495141f23cd9d3c9fe71d8268a7c9b217
Armand Dogaru <dogaruionelarmand@gmail.com>
2023-07-31 23:39:02 +0300

    Add email uniqueness

--- src/main/java/com/endava/bugHunting/bug_hunting/controller/UserController.java
+++ src/main/java/com/endava/bugHunting/bug_hunting/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.endava.bugHunting.bug_hunting.controller;
 
 import com.endava.bugHunting.bug_hunting.dto.UserDto;
-import com.endava.bugHunting.bug_hunting.entities.User;
 import com.endava.bugHunting.bug_hunting.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/users")
 public class UserController {
 
+    private static final String EMAIL_NOT_UNIQUE_MSG = "Email %s already in database";
     @Autowired
     private UserService userService;
 
     @PostMapping(value = "/register")
-    public User registerUserAccount(@RequestBody UserDto userDto) {
-        User save = userService.save(userDto);
-        return save;
+    public UserDto registerUserAccount(@RequestBody UserDto userDto) {
+//        Boolean emailPresent = isEmailPresent(userDto);
+//        if(emailPresent){
+//            throw new IllegalStateException(String.format(EMAIL_NOT_UNIQUE_MSG, userDto.getEmail()));
+//        }
+        UserDto savedUser = userService.save(userDto);
+        if (savedUser.getErrorMsg() != null) {
+            throw new IllegalStateException(savedUser.getErrorMsg());
+        }
+        return savedUser;
     }
+
+//    private Boolean isEmailPresent(UserDto userDto) {
+//        List<User> user = userService.findByEmail(userDto.getEmail());
+//        return user.size() > 0;
+//    }
 }
--- src/main/java/com/endava/bugHunting/bug_hunting/repository/UserRepository.java
+++ src/main/java/com/endava/bugHunting/bug_hunting/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.endava.bugHunting.bug_hunting.repository;
 import com.endava.bugHunting.bug_hunting.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User> findByEmail(String email);
 }
