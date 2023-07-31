f5336f7efd12c0a0fec91c134f372a84a41af538
Armand Dogaru <dogaruionelarmand@gmail.com>
2023-07-31 16:05:29 +0300

    Add register user functionality

--- src/main/resources/db/migration/V1_1_0__db_schema.sql
+++ src/main/resources/db/migration/V1_1_0__db_schema.sql
@@ -1,11 +1,9 @@
-CREATE TABLE users
+create TABLE users
 (
     user_id                bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     first_name             varchar(30)    NOT NULL,
     last_name              varchar(30)    NOT NULL,
-    email                  varchar(30)    NOT NULL,
+    email                  varchar(30)    NOT NULL UNIQUE,
     password               varchar(30)    NOT NULL,
-    role                   varchar(30)    CHECK (role IN ('ADMIN', 'USER')),
-    created_at             timestamp with time zone      NOT NULL DEFAULT (now() at time zone 'utc'),
-    updated_at             timestamp with time zone      NOT NULL DEFAULT (now() at time zone 'utc')
+    role                   varchar(30)    CHECK (role IN ('ADMIN', 'USER'))
 );
