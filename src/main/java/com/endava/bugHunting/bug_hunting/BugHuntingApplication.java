37a5a37fb054cbd283226386a38c33a3deea5698
Armand Dogaru <dogaruionelarmand@gmail.com>
2023-08-17 12:02:24 +0300

    Add Cors security configuration

--- src/main/java/com/endava/bugHunting/bug_hunting/BugHuntingApplication.java
+++ src/main/java/com/endava/bugHunting/bug_hunting/BugHuntingApplication.java
@@ -3,6 +3,9 @@ package com.endava.bugHunting.bug_hunting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class BugHuntingApplication {
@@ -10,5 +13,18 @@ public class BugHuntingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BugHuntingApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").
+						allowedOrigins("*").
+						allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+			}
+  		};
+	}
+
 
 }
