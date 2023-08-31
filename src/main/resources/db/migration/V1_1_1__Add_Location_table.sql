131b753388502ce389c1b4066849107a335be491
Armand Dogaru <dogaruionelarmand@gmail.com>
2023-08-31 11:31:12 +0300

    Refactor sql schema

--- src/main/resources/db/migration/V1_1_1__Add_Location_table.sql
+++ src/main/resources/db/migration/V1_1_1__Add_Location_table.sql
@@ -1,7 +1,7 @@
 create TABLE locations
 (
     id           bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    address      varchar(100) NOT NULL,
-    name         varchar(50) NOT NULL,
-    phone_number varchar(20) NOT NULL
+    address      varchar(100) NULL,
+    name         varchar(50)  NULL,
+    phone_number varchar(20)  NULL
 );
